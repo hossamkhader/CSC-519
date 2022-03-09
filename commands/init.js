d4eac9942ff193d25766d7b2287a3b759c49b903
Evan Kountouris <egkounto@ncsu.edu>
2022-03-09 17:30:32 -0500

    attempt OS check ...

--- commands/init.js
+++ commands/init.js
@@ -3,6 +3,7 @@ const path = require("path");
 const child = require("child_process");
 
 const buildSpec = require("../lib/buildSpec");
+const { stdout } = require("process");
 
 exports.command = "init";
 exports.desc = "Prepare tool";
@@ -14,8 +15,9 @@ exports.handler = async (argv) => {
   const { processor } = argv;
 
   console.log(chalk.green("Preparing computing environment..."));
-  console.log(chalk.green("--- Check bakerx is installed ---"));
 
+  // bakerx check
+  console.log(chalk.green("--- Check bakerx is installed ---"));
   child.exec("bakerx --version", (err, stdout, stderr) => {
     if (err) {
       console.error(err);
@@ -26,6 +28,7 @@ exports.handler = async (argv) => {
     console.log(chalk.green("bakerx is installed."));
   });
 
+  // image pull check
   console.log(chalk.green("--- Pulling ubuntu focal image ---"));
   child.exec(
     `bakerx pull focal cloud-images.ubuntu.com`,
@@ -39,6 +42,18 @@ exports.handler = async (argv) => {
       console.log(chalk.green("focal image pull returned with no errors."));
     }
   );
+
+  // OS check
+  console.log(chalk.green("--- Check OS ---"));
+  child.exec(`echo \$OSTYPE`, (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      console.log(chalk.red("FAILED: OS check"));
+      return;
+    }
+    console.log(stdout);
+    console.log(chalk.green("$OSTYPE:", stdout));
+  });
 
   //TODO: Ensure console output arrives in order i.e. read up on execSync
 
