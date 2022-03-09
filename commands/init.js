72e28dd8638cade9f382a1610b59b5523c1fd2ff
Evan Kountouris <egkounto@ncsu.edu>
2022-03-09 17:12:25 -0500

    check bakerx is installed

--- commands/init.js
+++ commands/init.js
@@ -1,34 +1,42 @@
-const chalk = require('chalk');
-const path = require('path');
-const child = require('child_process');
+const chalk = require("chalk");
+const path = require("path");
+const child = require("child_process");
 
-const buildSpec = require('../lib/buildSpec');
+const buildSpec = require("../lib/buildSpec");
 
-exports.command = 'init';
-exports.desc = 'Prepare tool';
-exports.builder = yargs => {
-    yargs.options({
-    });
+exports.command = "init";
+exports.desc = "Prepare tool";
+exports.builder = (yargs) => {
+  yargs.options({});
 };
 
+exports.handler = async (argv) => {
+  const { processor } = argv;
 
-exports.handler = async argv => {
+  console.log(chalk.green("Preparing computing environment..."));
+  console.log(chalk.green("--- Check bakerx is installed ---"));
 
-    const { processor } = argv;
+  child.exec("bakerx --version", (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      console.log(chalk.red("FAILED: bakerx is NOT installed."));
+      return;
+    }
+    console.log(stdout);
+    console.log(chalk.green("bakerx is installed."));
+  });
 
-    console.log(chalk.green("Preparing computing environment..."));
+  console.log(chalk.green("Pulling ubuntu focal image..."));
+  child.exec(`bakerx pull focal cloud-images.ubuntu.com`);
 
-    
-    console.log(chalk.green("Pulling ubuntu focal image..."));
-    child.exec(`bakerx pull focal cloud-images.ubuntu.com`);
+  console.log(chalk.green("Creating VM pipeline-vm..."));
+  child.execSync(`bakerx run pipeline-vm focal --memory 2048`);
 
-    console.log(chalk.green("Creating VM pipeline-vm..."));
-    child.execSync(`bakerx run pipeline-vm focal --memory 2048`);
+  let ssh_command_subprocess = child.exec(`bakerx ssh-info pipeline-vm`);
 
-    let ssh_command_subprocess = child.exec(`bakerx ssh-info pipeline-vm`);
-
-    ssh_command_subprocess.stdout.on('data', stdout => {
-        ssh_command = stdout.toString();
-        console.log( chalk.gray(ssh_command));
-    });
+  ssh_command_subprocess.stdout.on("data", (stdout) => {
+    ssh_command = stdout.toString();
+    console.log(chalk.gray(ssh_command));
+  });
 };
+
