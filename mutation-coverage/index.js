11a22e1780167938c281dd2742fc7df5ac9ee2c0
hossamkhader <hossamkhader@gmail.com>
2022-04-09 20:36:17 -0400

    add mutation-coverage code

---
+++ mutation-coverage/index.js
@@ -0,0 +1,61 @@
+const exec = require('child_process').exec;
+const chalk = require('chalk');
+const md5File = require('md5-file');
+
+
+let snapshots = [
+    'http://localhost:3000/survey/long.md', 
+    'http://localhost:3000/survey/upload.md', 
+    'http://localhost:3000/survey/survey.md', 
+    'http://localhost:3000/survey/variations.md'
+];
+
+async function _exec(command) {
+    return new Promise(function (resolve, reject) {
+        let subprocess = exec(`${command}`, {maxBuffer: 1024*5000});
+        subprocess.stdout.on('data', stdout => {
+            console.log( chalk.gray(stdout.toString() ));
+        });
+        subprocess.stderr.on('data', stderr => {
+            console.log( chalk.gray(stderr.toString() ));
+        });
+        // Subscribe to error starting process or process exiting events.
+        subprocess.on('error', err => {
+            console.log( chalk.red( err.message ) );
+            reject(err);
+        });
+        subprocess.on('exit', code => {
+            resolve(code);
+        });
+    });
+}
+
+async function run() {
+    await _exec('cd ../checkbox.io-micro-preview && git restore marqdown.js');
+    await _exec('cd ASTRewrite && node index.js');
+    let microservice = exec('node index.js', {cwd: '../checkbox.io-micro-preview'});
+    await _exec('rm -f snapshots/tmp/*');
+    for (let snapshot of snapshots) {
+        let file_name = snapshot.split('/')[4].split('.')[0]
+        await _exec(`../screenshot/screenshot.js ${snapshot} snapshots/tmp/${file_name}`);
+        console.log(md5File.sync(`snapshots/tmp/${file_name}.png`), md5File.sync(`snapshots/baseline/${file_name}.png`));
+        if (md5File.sync(`snapshots/tmp/${file_name}.png`) != md5File.sync(`snapshots/baseline/${file_name}.png`)) {
+            console.log(`TEST FAIL for ${file_name}`);
+        }
+    }
+    microservice.kill();
+    await _exec('cd ../checkbox.io-micro-preview && git restore marqdown.js');
+};
+
+
+async function main() {
+
+    for (i=0; i < 1000; i++) {
+        await run();
+    }
+}
+
+
+if (require.main === module) {
+    main();
+}
