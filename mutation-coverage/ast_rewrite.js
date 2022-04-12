319396a66a4fec948bc15e5624fc30545ba98202
hkhader <hkhader@ncsu.edu>
2022-04-11 23:23:20 -0400

    implement the 8 mutation opertations

--- mutation-coverage/ast_rewrite.js
+++ mutation-coverage/ast_rewrite.js
@@ -5,7 +5,16 @@ const options = {tokens:true, tolerant: true, loc: true, range: true };
 const fs = require("fs");
 const chalk = require('chalk');
 
-let operations = [ NegateConditionals, ConditionalBoundary ]
+let operations = [ 
+    NegateConditionals, 
+    ConditionalBoundary, 
+    IncrementalMutations, 
+    ConditionalExpression, 
+    //NonEmptyString, 
+    //MutateControlFlow,
+    ConstantReplacement, 
+    CloneReturnEarly
+]
 
 function rewrite( filepath, newPath ) {
 
@@ -58,6 +67,193 @@ function NegateConditionals(ast) {
                     node.operator = "==";
                     change = `Replacing != with == on line ${node.loc.start.line}`;
                 }
+            }
+            current++;
+        }
+    })
+    return change;
+}
+
+function IncrementalMutations(ast) {
+
+    let candidates = [];
+    let change;
+
+    let i = 0;
+    traverseWithParents(ast, (node) => {
+        if( node.type === "UpdateExpression" ) {
+            if ( ["++", "--",].includes(node.operator) ) {
+                candidates.push(i);
+            }
+            i++;
+        }
+    })
+
+    let mutateTarget = candidates[getRandomInt(candidates.length)];
+    let current = 0;
+    traverseWithParents(ast, (node) => {
+        if( node.type === "UpdateExpression" ) {
+            if( current === mutateTarget ) {
+                if( node.prefix == true ) {
+                    change = `Replacing ${node.operator}${node.argument.name} with ${node.argument.name}${node.operator} on line ${node.loc.start.line}`;
+                }
+                else {
+                    change = `Replacing ${node.argument.name}${node.operator} with ${node.operator}${node.argument.name} on line ${node.loc.start.line}`;
+                }
+                node.prefix = !node.prefix
+            }
+            current++;
+        }
+    })
+    return change;
+}
+
+function ConditionalExpression(ast) {
+
+    let candidates = [];
+    let change;
+
+    let i = 0;
+    traverseWithParents(ast, (node) => {
+        if( node.type === "LogicalExpression" ) {
+            if ( ["&&", "||",].includes(node.operator) ) {
+                candidates.push(i);
+            }
+            i++;
+        }
+    })
+
+    let mutateTarget = candidates[getRandomInt(candidates.length)];
+    let current = 0;
+    traverseWithParents(ast, (node) => {
+        if( node.type === "LogicalExpression" ) {
+            if( current === mutateTarget ) {
+                if( node.operator === "&&" ) {
+                    change = `Replacing && with || on line ${node.loc.start.line}`;
+                    node.operator = "||";
+                }
+                    
+                else if ( node.operator === "||" ) {
+                    change = `Replacing || with && on line ${node.loc.start.line}`;
+                    node.operator = "&&";
+                }
+            }
+            current++;
+        }
+    })
+    return change;
+}
+
+function MutateControlFlow(ast) {
+
+    let candidates = [];
+    let change;
+
+    let i = 0;
+    traverseWithParents(ast, (node) => {
+        if( "alternate" in node && node.type === "BlockStatement" ) {
+            candidates.push(i);
+            i++;
+        }
+    })
+
+    let mutateTarget = candidates[getRandomInt(candidates.length)];
+    let current = 0;
+    traverseWithParents(ast, (node) => {
+        if( "alternate" in node && node.type === "BlockStatement" ) {
+            if( current === mutateTarget ) {
+                change = `Replacing "if" with "else if" on line ${node.loc.start.line}`;
+                node.type = "IfStatement";
+            }
+            current++;
+        }
+    })
+    return change;
+}
+
+function NonEmptyString(ast) {
+
+    let candidates = [];
+    let change;
+
+    let i = 0;
+    traverseWithParents(ast, (node) => {
+        if( node.type === "Literal" ) {
+            if ( node.value === "" ) {
+                candidates.push(i);
+            }
+            i++;
+        }
+    })
+
+    let mutateTarget = candidates[getRandomInt(candidates.length)];
+    let current = 0;
+    traverseWithParents(ast, (node) => {
+        if( node.type === "Literal" ) {
+            if( current === mutateTarget ) {
+                change = `Replacing "" with "<div>Bug</div>" on line ${node.loc.start.line}`;
+                node.value = "<div>Bug</div>";
+            }
+            current++;
+        }
+    })
+    return change;
+}
+
+function CloneReturnEarly(ast) {
+
+    let candidates = [];
+    let change;
+
+    let i = 0;
+    traverseWithParents(ast, (node) => {
+        if( node.type === "FunctionDeclaration" ) {
+            traverseWithParents(node, (subnode) => {
+                if ( subnode.type === "ReturnStatement" && subnode.argument.name === "embeddedHtml" ) {
+                    candidates.push(i);
+                }
+            })
+            i++;
+        }
+    })
+
+    let mutateTarget = candidates[getRandomInt(candidates.length)];
+    let current = 0;
+    traverseWithParents(ast, (node) => {
+        if( node.type === "FunctionDeclaration" ) {
+            if( current === mutateTarget ) {
+                var randomLocation = getRandomInt(node.body.body.length);
+                node.body.body.splice(randomLocation, 0, {"type": "ReturnStatement","argument": {"type": "Identifier","name": "embeddedHtml"}});
+                change = `Inserting "return embeddedHtml;" on line ${node.loc.start.line + randomLocation}`;
+            }
+            current++;
+        }
+    })
+    return change;
+}
+
+function ConstantReplacement(ast) {
+
+    let candidates = [];
+    let change;
+
+    let i = 0;
+    traverseWithParents(ast, (node) => {
+        if( node.type === "Literal" ) {
+            if ( node.value == 0 ) {
+                candidates.push(i);
+            }
+            i++;
+        }
+    })
+
+    let mutateTarget = candidates[getRandomInt(candidates.length)];
+    let current = 0;
+    traverseWithParents(ast, (node) => {
+        if( node.type === "Literal" ) {
+            if( current === mutateTarget ) {
+                change = `Replacing 0 with 3 on line ${node.loc.start.line}`;
+                node.value = 3;
             }
             current++;
         }
